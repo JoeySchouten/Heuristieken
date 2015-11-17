10b85fc5419ba27c6824ddf72823e211b7b03072
KayScouse <kayleighschoorl@hotmail.com>
2015-11-17 16:21:19 +0100

    de PlaceAll functie omgebouwd

--- combination.py
+++ combination.py
@@ -25,32 +25,33 @@ class Combination(object):
         crawler = Point(0,0)
         # loop to place houses
         for i in range(len(self.houses)):
+            reqspacex=2*self.houses[i].minVrij+self.houses[i].width
+            reqspacey=2*self.houses[i].minVrij+self.houses[i].length
             while self.houses[i].geplaatst == False:
-                reqspacex=(2*self.houses[i].minVrij+self.houses[i].width)
-                reqspacey=(2*self.houses[i].minVrij+self.houses[i].length)
-                # check if crawler point is empty and if house can be placed (move to own boolean fucntion?)
-                # TODO: does not check whether the min. free space is honoured
-                # TODO: fill map data
-                if (self.map.data.get(crawler.x, crawler.y) and
-                crawler.x + reqspacex <= self.map.width and
-                crawler.y + reqspacey <= self.map.length):
-                    self.houses[i].place(crawler.x, crawler.y)
-                    print(crawler.x, crawler.y)
-                    # offsets dictionary key to include vrijstand
-                    keyx = crawler.x-self.houses[i].minVrij
-                    keyy = crawler.y-self.houses[i].minVrij
-                    self.map.fill(keyx, keyy, self.houses[i].minVrij, self.houses[i].width, self.houses[i].length)
-                    crawler.setPoint(crawler.x+reqspacex, crawler.y)
-                # else move crawler point and try again; if crawler is out of bounds, set x to 0, y++ (also make its own function?)
-                else:
-                    crawler.setPoint(crawler.x+1,crawler.y)
-                    if crawler.x > self.map.width:
-                        crawler.setPoint(0,crawler.y+1)
+                print crawler.x, crawler.y
+                # check if crawler point exists
+                if self.map.data.get(crawler.x, crawler.y) != None:
+                    # if crawler point is not empty, go to next point
+                    if (self.map.data.get(crawler.x, crawler.y) == "huis" or
+                        self.map.data.get(crawler.x, crawler.y) == "moetvrij"):
+                        crawler.setPoint(crawler.x+1, crawler.y)
+                        if crawler.x > self.map.width:
+                            crawler.setPoint(0, crawler.y+1)
+                    # else if there is enough space, place house
+                    elif crawler.x + reqspacex <= self.map.width and crawler.y + reqspacey <= self.map.length:
+                        self.houses[i].place(crawler.x, crawler.y)
+                        # offsets dictionary key to include vrijstand
+                        keyx = crawler.x-self.houses[i].minVrij
+                        keyy = crawler.y-self.houses[i].minVrij
+                        self.map.fill(keyx, keyy, self.houses[i].minVrij, self.houses[i].width, self.houses[i].length)
+                        crawler.setPoint(crawler.x+reqspacex, crawler.y)
+                    # else go to next row
+                    else:
+                        crawler.setPoint(0, crawler.y+1)
 
     #function to print to csv file for visualisation
     # output: corner x, corner y, length, width, type
     def printToCSV(self):
-        output = open("output.csv", "w")
         output = open("output.csv", "wb")
         try:
             writer = csv.writer(output)
