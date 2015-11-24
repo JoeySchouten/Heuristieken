8d4a2ddf7bc91f9c835b9017c14ef5c4f5f720a1
KayScouse <kayleighschoorl@hotmail.com>
2015-11-24 15:33:15 +0100

    commit

--- vrijstand.py
+++ vrijstand.py
@@ -10,7 +10,6 @@
 	ga +1 verder dan vorige ronde
 	doe dit zolang er geen huizen geraakt worden
 	sla de vrijstand op in een variabele, trek daar de min vrijstaande waarde bij af
-
 	Maak er 1 functie van die je over een object trekt; een for loop die over de array met huizen gaat is zo gebouwd en is wss beter buiten de functie te plaatsen.
 
     # functie voor het berekenen van de vrijstand van een huis
