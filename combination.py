f930daa67f1e24e787c7e3d03d36b6a0ce762597
KayScouse <kayleighschoorl@hotmail.com>
2015-12-08 15:40:03 +0100

    swap zit in een apart bestand

--- combination.py
+++ combination.py
@@ -199,68 +199,6 @@ class Combination(object):
         return True
 
 
-    # function to shuffle a house
-    def swapHouse(self, huis1, huis2):
-        huis2 = self.houses[huis2]
-        mogelijk = True
-
-        xH1 = huis1.hoekpunt.x
-        yH1 = huis1.hoekpunt.y
-        xH2 = huis2.hoekpunt.x
-        yH2 = huis2.hoekpunt.y
-
-        # check if house 1 has enough space
-        for i in range (len(self.houses)):
-            if i == huis2:
-                pass
-            else:
-                xiH1 = self.houses[i].hoekpunt.x
-                yiH1 = self.houses[i].hoekpunt.y
-                #check if H1 left and right corners are within iH1's x-range
-                if (xH1 - huis1.minVrij <= xiH1 <= xH1 + huis1.width + huis1.minVrij) or (xH1 - huis1.minVrij <= xiH1 + self.houses[i].width <= xH1 + huis1.width + huis1.minVrij):
-                    #check if H1 top and bottom corners are within iH1's y-range
-                    if (yH1 - huis1.minVrij <= yiH1 <= yH1 + huis1.length + huis1.minVrij) or (yH1 - huis1.minVrij <= yiH1 + self.houses[i].length <= yH1 + huis1.length + huis1.minVrij):
-                        mogelijk = False
-                #check if iH1 left and right corners are within H1's x-range
-                if (xiH1 - self.houses[i].minVrij <= xH1 <= xiH1 + self.houses[i].width + self.houses[i].minVrij) or (xiH1 - self.houses[i].minVrij <= xH1+huis.width <= xiH1 + self.houses[i].width + self.houses[i].minVrij):
-                    #check if iH1 top and bottom corners are within H1's y-range
-                    if (yiH1 - self.houses[i].minVrij <= yH1 <= yiH1 + self.houses[i].length + self.houses[i].minVrij) or (yiH1 - self.houses[i].minVrij <= yH1+huis.length <= yiH1 + self.houses[i].length + self.houses[i].minVrij):
-                        mogelijk = False
-
-        # check if house 2 has enough space
-        for i in range (len(self.houses)):
-            if i == huis1:
-                pass
-            else:
-                xiH2 = self.houses[i].hoekpunt.x
-                yiH2 = self.houses[i].hoekpunt.y
-                #check if H2 left and right corners are within iH2's x-range
-                if (xH2 - huis2.minVrij <= xiH2 <= xH2 + huis2.width + huis2.minVrij) or (xH2 - huis2.minVrij <= xiH2 + self.houses[i].width <= xH2 + huis2.width + huis2.minVrij):
-                    #check if H2 top and bottom corners are within H1's y-range
-                    if (yH2 - huis2.minVrij <= yiH2 <= yH2 + huis2.length + huis2.minVrij) or (yH2 - huis2.minVrij <= yiH2 + self.houses[i].length <= yH2 + huis2.length + huis2.minVrij):
-                        mogelijk = False
-                #check if iH2 left and right corners are within H2's x-range
-                if (xiH2 - self.houses[i].minVrij <= xH2 <= xiH2 + self.houses[i].width + self.houses[i].minVrij) or (xiH2 - self.houses[i].minVrij <= xH2 + huis.width <= xiH2 + self.houses[i].width + self.houses[i].minVrij):
-                    #check if H1 top and bottom corners are within H2's y-range
-                    if (yiH2 - self.houses[i].minVrij <= yH2 <= yiH2 + self.houses[i].length + self.houses[i].minVrij) or (yiH2 - self.houses[i].minVrij <= yH2 + huis.length <= yiH2 + self.houses[i].length + self.houses[i].minVrij):
-                        mogelijk = False
-
-        if mogelijk == True:
-            # swap x coordinates
-            tempx = xH1
-            xH1 = xH2
-            xH2 = tempx
-
-            # swap y coordinates
-            tempy = yH
-            yH1 = yH2
-            yH2 = tempy
-
-            # set new coordinates
-            huis1.hoekpunt.setPoint(xH1, yH1)
-            huis2.hoekpunt.setPoint(xH2, yH2)
-
-
     #function to print to csv file for visualisation
     # output: corner x, corner y, length, width, type
     def printToCSV(self):
