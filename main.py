67e0471952b016f0f447cf998296a74954084de6
JoeySchouten <joey.schouten@gmail.com>
2015-12-08 16:56:12 +0100

    commandlineargs toegevoegd; iteratie aan grafiek toegevoegd

--- main.py
+++ main.py
@@ -7,32 +7,6 @@ from graph import *
 from schuiven import schuiven
 from swap import swapHouse
 
-toegestanehuizen = [20,40,60]
-toegestanemethoden = ["randsample", "schuiven", "swappen"]
-aantalhuizen = int(sys.argv[1])
-aantalwater = 4
-
-uitkomsten = []
-hoogstewaarde = 0
-iteratie = 0
-best = 0
-
-#sys.argv[] -> 1: aantal huizen; 2:methode
-if int(sys.argv[1]) not in toegestanehuizen:
-    print "Incorrect aantal huizen opgegeven. Correct gebruik:"
-    print "python main.py [aantal huizen (20, 40, of 60)] [methode (randsample, schuiven, swappen)]"
-    sys.exit()
-elif str(sys.argv[2]) not in toegestanemethoden:
-    print "Incorrecte methode opgegeven. Correct gebruik:"
-    print "python main.py [aantal huizen (20, 40, of 60)] [methode (randsample, schuiven, swappen)]"
-    sys.exit()
-
-# bouwen grafiek
-plt.xlabel('Iteraties')
-plt.ylabel('Waarde in Euro\'s')
-plt.ion()
-plt.show()
-
 def createRandom():
     error = True
     while error == True:
@@ -54,6 +28,44 @@ def createRandom():
             plt.savefig('graph.png', dpi=300, bbox_inches='tight')
     return combinatie
 
+def informWrongUsage():
+    print "Incorrect aantal huizen opgegeven. Correct gebruik:"
+    print "python main.py [aantal huizen] [methode] [score]"
+    print "aantal huizen: 20, 40 of 60"
+    print "methode: randsample, schuiven of swappen"
+    print "score: waarde of vrijstand"
+    sys.exit()
+
+toegestanehuizen = [20,40,60]
+toegestanemethoden = ["randsample", "schuiven", "swappen"]
+toegestanescore = ["waarde", "vrijstand"]
+aantalhuizen = int(sys.argv[1])
+aantalwater = 4
+
+uitkomsten = []
+hoogstewaarde = 0
+iteratie = 0
+best = 0
+# 0 = scoren op vrijstand; 1 = scoren op waarde in euro's
+criterium = 0
+
+#sys.argv[] -> 1: aantal huizen; 2:methode; 3: score criterium
+if int(sys.argv[1]) not in toegestanehuizen:
+    informWrongUsage()
+elif str(sys.argv[2]) not in toegestanemethoden:
+    informWrongUsage()
+elif str(sys.argv[3]) not in toegestanescore:
+    informWrongUsage()
+
+if str(sys.argv[3]) == "waarde":
+    criterium = 1
+
+# bouwen grafiek
+plt.xlabel('Iteraties')
+plt.ylabel('Waarde in Euro\'s')
+plt.ion()
+plt.show()
+
 if sys.argv[2] == "randsample":
     plt.title('Amstelhaege Random Sampling')
     while True:
@@ -65,14 +77,14 @@ if sys.argv[2] == "randsample":
         if error == False:
             combinatie.evalueer()
             temp = combinatie.evaluatie
-            if temp[1] > hoogstewaarde:
-                hoogstewaarde = temp[1]
+            if temp[criterium] > hoogstewaarde:
+                hoogstewaarde = temp[criterium]
                 best = combinatie
                 best.evalueer()
         uitkomsten.append(hoogstewaarde)
         plt.plot(iteratie, hoogstewaarde, '.-r')
         plt.draw()
-        plt.suptitle("Hoogste huidige waarde is: " + str(hoogstewaarde), fontsize=13)
+        plt.suptitle("Hoogste huidige waarde: " + str(hoogstewaarde) + " Huidige iteratie: " + str(iteratie), fontsize=13)
         plt.savefig('graph.png', dpi=300, bbox_inches='tight')
         iteratie += 1
 
@@ -84,14 +96,14 @@ elif str(sys.argv[2]) == "schuiven":
         if schuiven(combinatie) == True:
             combinatie.evalueer()
             temp = combinatie.evaluatie
-            if temp[1] > hoogstewaarde:
-                hoogstewaarde = temp[1]
+            if temp[criterium] > hoogstewaarde:
+                hoogstewaarde = temp[criterium]
                 best = combinatie
                 best.evalueer()
         uitkomsten.append(hoogstewaarde)
         plt.plot(iteratie, hoogstewaarde, '.-r')
         plt.draw()
-        plt.suptitle("Hoogste huidige waarde is: " + str(hoogstewaarde), fontsize=13)
+        plt.suptitle("Hoogste huidige waarde: " + str(hoogstewaarde) + " Huidige iteratie: " + str(iteratie), fontsize=13)
         plt.savefig('graph.png', dpi=300, bbox_inches='tight')
         iteratie += 1
 
@@ -103,13 +115,13 @@ elif str(sys.argv[2]) == "swappen":
         if swapHouse(combinatie) == True:
             combinatie.evalueer()
             temp = combinatie.evaluatie
-            if temp[1] > hoogstewaarde:
-                hoogstewaarde = temp[1]
+            if temp[criterium] > hoogstewaarde:
+                hoogstewaarde = temp[criterium]
                 best = combinatie
                 best.evalueer()
         uitkomsten.append(hoogstewaarde)
         plt.plot(iteratie, hoogstewaarde, '.-r')
         plt.draw()
-        plt.suptitle("Hoogste huidige waarde is: " + str(hoogstewaarde), fontsize=13)
+        plt.suptitle("Hoogste huidige waarde: " + str(hoogstewaarde) + " Huidige iteratie: " + str(iteratie), fontsize=13)
         plt.savefig('graph.png', dpi=300, bbox_inches='tight')
         iteratie += 1
